dd54d1ca2cff8d1140757bc11c3e2c58a464d21d
dhavalbathe <dhavalbathe@gmail.com>
2026-07-22 19:25:10 +0530

    added AuthenticationManager in SecurityConfig class

--- src/main/java/com/SpringBoot/AuthManagement/config/SecurityConfig.java
+++ src/main/java/com/SpringBoot/AuthManagement/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.SpringBoot.AuthManagement.config;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,5 +32,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 }
